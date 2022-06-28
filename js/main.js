837d0417ccb2beb469ddb6a42594abd07f1eac0d
Gabriel <gabrieltreppo3@gmail.com>
2022-06-28 18:34:12 -0300

    agregadas carpetas: css, img, js y pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let nombre = prompt("Ingrese su nombre")
+    console.log(nombre)
+
+let edad = prompt("Ingrese una edad")
+
+if(edad > 30){
+    console.warn("Bienvenido. Buenos dias")
+
+} else {
+    console.error("Hola, como andas")
+} 
